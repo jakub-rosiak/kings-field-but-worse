8fdf9a7a036a984304627dd8c29878fbc2409fb7
Jakub Rosiak <251620@edu.p.lodz.pl>
2026-01-15 19:09:52 +0100

    feat: add  plugin

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,7 +1,20 @@
 use bevy::prelude::*;
+use input::{OKInputPlugin, events::input_intent::InputIntent, resources::plugin_config::InputPluginConfig};
+use log::debug;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(OKInputPlugin::new(InputPluginConfig::default()))
+        .add_systems(Update, log_input)
         .run();
+}
+
+
+fn log_input(mut intent: MessageReader<InputIntent>) {
+    let event = intent.read();
+    for ev in event {
+        debug!("{:?}", ev);
+    }
+    
 }
