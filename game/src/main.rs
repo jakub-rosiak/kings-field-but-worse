0e9c472a7fa0b210b4bfac391769545c4d576b1f
Jakub Niekraszewicz <niekraszewiczjakub@proton.me>
2026-01-16 00:47:24 +0100

    chore: create branch

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,31 +1,27 @@
 use avian3d::PhysicsPlugins;
 use bevy::prelude::*;
-use avian3d::prelude::*;
-use input::{OKInputPlugin, events::input_intent::InputIntent, resources::plugin_config::InputPluginConfig};
+use input::{
+    OKInputPlugin, events::input_intent::InputIntent, resources::plugin_config::InputPluginConfig,
+};
 use log::debug;
+
+use crate::pooga::Pooga;
+
+mod pooga;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(OKInputPlugin::new(InputPluginConfig::default()))
         .add_plugins(PhysicsPlugins::default())
-        .add_systems(Startup, setup)
+        .add_plugins(Pooga::new())
         .add_systems(Update, log_input)
         .run();
 }
-
 
 fn log_input(mut intent: MessageReader<InputIntent>) {
     let event = intent.read();
     for ev in event {
         debug!("{:?}", ev);
     }
-    
-}
-
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
 }
